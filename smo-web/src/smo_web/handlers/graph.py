9b9b18e8b14da824ebf55662ee9bcff986f3588d
Stefane Fermigier <sf@abilian.com>
2025-07-08 16:14:17 +0200

    feat: add missing operation.

--- smo-web/src/smo_web/handlers/graph.py
+++ smo-web/src/smo_web/handlers/graph.py
@@ -3,13 +3,12 @@ from smo_web.util import get_core_context, get_db_session
 from smo_core.services.hdag import graph_service
 
 
-def list(project):
+def get_all_for_project(project):
     """Handler for GET /project/{project}/graphs."""
-    context = get_core_context()
     db_session = get_db_session()
 
-    graphs = graph_service.fetch_project_graphs(context, db_session, project)
-    return [graph.to_dict() for graph in graphs], 200
+    graphs = graph_service.fetch_project_graphs(db_session, project)
+    return [graph for graph in graphs], 200, {"Content-Type": "application/json"}
 
 
 def deploy(project, body):
@@ -36,7 +35,7 @@ def get_graph(name):
     db_session = get_db_session()
     graph = graph_service.fetch_graph(db_session, name)
     if graph:
-        return graph.to_dict(), 200
+        return graph.to_dict(), 200, {"Content-Type": "application/json"}
     raise ValueError(f"Graph with name {name} not found")
 
 
@@ -44,21 +43,21 @@ def remove(name):
     context = get_core_context()
     db_session = get_db_session()
     graph_service.remove_graph(context, db_session, name)
-    return f"Graph {name} removed successfully", 200
+    return f"Graph {name} removed successfully", 200, {"Content-Type": "application/json"}
 
 
 def start(name):
     context = get_core_context()
     db_session = get_db_session()
     graph_service.start_graph(context, db_session, name)
-    return f"Graph {name} start triggered successfully", 200
+    return f"Graph {name} start triggered successfully", 200, {"Content-Type": "application/json"}
 
 
 def stop(name):
     context = get_core_context()
     db_session = get_db_session()
     graph_service.stop_graph(context, db_session, name)
-    return f"Graph {name} stop triggered successfully", 200
+    return f"Graph {name} stop triggered successfully", 200, {"Content-Type": "application/json"}
 
 
 def placement(name):
