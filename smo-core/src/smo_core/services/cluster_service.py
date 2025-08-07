7ca2898c334e46d784b355b8f9165213100a4f12
Stefane Fermigier <sf@abilian.com>
2025-08-07 21:39:29 +0200

    refact: split large method

--- smo-core/src/smo_core/services/cluster_service.py
+++ smo-core/src/smo_core/services/cluster_service.py
@@ -27,32 +27,35 @@ class ClusterService:
         Retrieves all cluster data from Karmada, syncs with the DB,
         and creates Grafana dashboards if needed.
         """
-        cluster_dict = []
+        cluster_dicts = []
         karmada_cluster_info = self.karmada_helper.get_cluster_info()
 
         for cluster_name, info in karmada_cluster_info.items():
-            stmt = select(Cluster).where(Cluster.name == cluster_name)
-            cluster = self.db_session.scalars(stmt).first()
-            if cluster is not None:
-                cluster.available_cpu = info["remaining_cpu"]
-                cluster.available_ram = info["remaining_memory_bytes"]
-                cluster.availability = info["availability"]
-            else:
-                dashboard = self.grafana_helper.create_cluster_dashboard(cluster_name)
-                response = self.grafana_helper.publish_dashboard(dashboard)
-                grafana_url = f"{self.config['grafana']['host']}{response['url']}"
-                cluster = Cluster(
-                    name=cluster_name,
-                    location="Unknown",
-                    available_cpu=info["remaining_cpu"],
-                    available_ram=info["remaining_memory_bytes"],
-                    availability=info["availability"],
-                    acceleration=False,  # TODO: A mechanism to discover this is needed
-                    grafana=grafana_url,
-                )
-                self.db_session.add(cluster)
+            cluster = self.update_cluster(cluster_name, info)
+            cluster_dicts.append(cluster.to_dict())
 
-            self.db_session.commit()
-            cluster_dict.append(cluster.to_dict())
+        self.db_session.commit()
+        return cluster_dicts
 
-        return cluster_dict
+    def update_cluster(self, cluster_name: str, info: dict) -> Cluster:
+        stmt = select(Cluster).where(Cluster.name == cluster_name)
+        cluster = self.db_session.scalars(stmt).first()
+        if cluster is not None:
+            cluster.available_cpu = info["remaining_cpu"]
+            cluster.available_ram = info["remaining_memory_bytes"]
+            cluster.availability = info["availability"]
+        else:
+            dashboard = self.grafana_helper.create_cluster_dashboard(cluster_name)
+            response = self.grafana_helper.publish_dashboard(dashboard)
+            grafana_url = f"{self.config['grafana']['host']}{response['url']}"
+            cluster = Cluster(
+                name=cluster_name,
+                location="Unknown",
+                available_cpu=info["remaining_cpu"],
+                available_ram=info["remaining_memory_bytes"],
+                availability=info["availability"],
+                acceleration=False,  # TODO: A mechanism to discover this is needed
+                grafana=grafana_url,
+            )
+            self.db_session.add(cluster)
+        return cluster
